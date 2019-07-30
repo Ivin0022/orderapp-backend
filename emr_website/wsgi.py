77f2e4cc3a144cf5e4b75013444b3fc48118c5d5
ivin stephen <ivinstephen001@gmail.com>
2019-07-31 01:09:46 +0530

    changed the name
    changed the name because, this application doesn't just take orders so the name seemed ill fixing

--- emr_website/wsgi.py
+++ emr_website/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orderApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emr_website.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orderApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emr_website.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
