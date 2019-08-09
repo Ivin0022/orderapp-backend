f7c6948e035a3ff958d0886fe817a2bb464fb04d
ivin stephen <ivinstephen001@gmail.com>
2019-08-10 02:34:13 +0530

    the status will automatically change to seen when enter details view

--- orders/views.py
+++ orders/views.py
@@ -12,6 +12,22 @@ class OrderListView(ListView):
 class OrderDetailView(DetailView):
     model = Order
 
+    def get_context_data(self, **kwargs):
+        """ used to update the status to seen when the details
+            of the current object are view
+            basically a hack so i don't have to write a 
+            function based view to handle the both rendering
+            the details and updating status 
+        """
+        current_object: Order = kwargs['object']
+
+        # don't set the status to seen if already fulfilled
+        print(current_object.status)
+        if current_object.status != Order.STAUTS_FULFILLED:
+            Order.set_status(current_object.pk, Order.STAUTS_SEEN)
+
+        return super().get_context_data(**kwargs)
+
 
 def hander(request, pk):
     Order.set_status(pk, Order.STAUTS_FULFILLED)
