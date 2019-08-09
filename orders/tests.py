4963f94ff5b638e90e86d7355396a5e2efe35928
ivin stephen <ivinstephen001@gmail.com>
2019-08-09 14:08:53 +0530

    wrote a basic test to understand testing in django
    wrote a test to see if the string representation of the model order is correct

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Order
+from patients.models import Patient
+
+
+class OrderModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        p = Patient(name='django', case_no='170009')
+        cls.order = Order(patient=p)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.order), 'django [170009]')
