426972dcb49cba33978236a7f367d339237bc4cd
ivin stephen <ivinstephen001@gmail.com>
2019-08-24 01:36:40 +0530

    added rest of the fields in patients

--- patients/models.py
+++ patients/models.py
@@ -1,6 +1,7 @@
 """ doc for modles in patients """
 
 from django.db import models
+from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -9,7 +10,12 @@ class Patient(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField('Age')
     case_no = models.CharField(max_length=6, unique=True)
+    next_of_kin = models.CharField(max_length=50, null=True)
+    date = models.DateField('date', default=timezone.now())
+    occupation = models.CharField(max_length=50, null=True)
     mobile = PhoneNumberField()
+    address = models.TextField(default='')
+    email = models.EmailField(null=True)
 
     def __str__(self):
         return f'{self.name} ({self.age})'
